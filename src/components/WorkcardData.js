1becc108f9d513f9a02ba57b2cf9a0e0ca4d88f6
kumaresan9176773252 <75007601+kumaresan9176773252@users.noreply.github.com>
2024-08-23 20:59:03 +0530

    Initial commit

---
+++ src/components/WorkcardData.js
@@ -0,0 +1,53 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+import pro4 from "../assets/project4.png";
+import pro5 from "../assets/projects 5.png";
+import pro6 from "../assets/project6.png";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Ecommerce Website Design",
+    text: "Welcome to our eCommerce website! We have a wide range of high-quality products, from clothing and electronics to home appliances. Our website is easy to navigate, and we offer secure checkout and fast shipping. Our customer service team is here to help you with any questions or concerns. Shop with us today and discover endless possibilities!",
+    view: "https://kumaresan9176773252.github.io/E-commerce-website/",
+    source: "https://github.com/kumaresan9176773252/E-commerce-website.git",
+  },
+  {
+    imgsrc: pro2,
+    title: "Trip Website Design",
+    text: "Welcome to our trip website! We offer a wide range of travel options, including flights, accommodations, tours, and activities. Our website is user-friendly and helps you compare prices and read reviews. Our team of travel experts is available to assist you with any questions or concerns. Let us help you plan your next dream vacation!",
+    view: "https://1618sr.csb.app/",
+    source: "https://github.com/kumaresan9176773252/Trip-Website.git",
+  },
+  {
+    imgsrc: pro3,
+    title: "car selling website design",
+    text: "Welcome to our car selling website! We offer a wide selection of high-quality cars, trucks, SUVs, and more. Our website is user-friendly and allows you to easily search and compare prices. Our team of experts is available to help you with any questions or concerns. Find your perfect vehicle today!",
+    view: "https://kumaresan9176773252.github.io/cars-project/",
+    source: "https://github.com/kumaresan9176773252/cars-project.git",
+  },
+  {
+    imgsrc: pro5,
+    title: "Blog Website Design",
+    text: "This blog website is built using the MERN stack (MongoDB, Express.js, React.js, Node.js) to provide a modern, responsive platform for users to create, read, update, and delete blog posts. The front end is designed with React.js, ensuring a seamless and dynamic user experience. On the back end, Node.js and Express.js handle server-side operations, while MongoDB manages the storage of blog data. The website features user-friendly navigation, rich text editing capabilities, and a secure database to store and manage content efficiently.",
+    view: "https://github.com/kumaresan9176773252/Blog-App.git",
+    source: "https://github.com/kumaresan9176773252/Blog-App.git",
+  },
+  {
+    imgsrc: pro4,
+    title: "AUTHENTICATION Design",
+    text: "This authentication website is developed using the MERN stack (MongoDB, Express.js, React.js, Node.js) to provide a secure and efficient user authentication system. The front end, built with React.js, offers a responsive and interactive interface for users to register, log in, and manage their accounts. On the back end, Node.js and Express.js are employed to handle authentication processes, including password hashing and token generation. MongoDB serves as the database, securely storing user credentials and session information. The website ensures robust security features, including JWT-based authentication and input validation, to protect user data.",
+    view: "https://github.com/kumaresan9176773252/AUTHENTICATION.git",
+    source: "https://github.com/kumaresan9176773252/AUTHENTICATION.git",
+  },
+  {
+    imgsrc: pro6,
+    title: "Book Store Website Design",
+    text: "This bookstore website, built using the MERN stack (MongoDB, Express.js, React.js, Node.js), offers a modern platform for browsing, purchasing, and managing books. The front end, developed with React.js, provides a responsive interface for exploring book categories, searching titles, and viewing detailed information. The back end, powered by Node.js and Express.js, manages user accounts, orders, and inventory, while MongoDB efficiently handles data storage. Key features include a shopping cart, wishlist, and secure payment gateway for a seamless online shopping experience.",
+    view: "https://book-store-frontend-hx58.onrender.com",
+    source: "https://github.com/kumaresan9176773252/Book-Store.git",
+  },
+];
+
+export default ProjectCardData;
